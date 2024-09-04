c657e0fd731d8bf27193d8a335820df2b5e1d096
Aldahir <oyuri4675@gmail.com>
2024-09-04 07:36:19 -0400

    First Commit

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work primarily in backend with Node.js and Express.
+    I've developed several APIs and services in these languages. I also enjoy working with databases like MongoDB and PostgreSQL.
+    Here is my <a href="https://github.com/Hoinkyoma37" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Computer Science degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree, but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch series on Netflix.
+  I also make backend programming tutorials on YouTube. Subscribe to <a href="https://youtube.com/@aldahirdev" target="_blank">my channel</a>! (If you like the content)
+  You'll learn how I built this portfolio you're currently viewing!`,
+  tv: `That's my TV. I've been watching a lot of tech YouTubers lately like:
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This is where I sleep. Great ideas come when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/aldahirdev/Resume/blob/main/Aldahir%20Resume.pdf" target="_blank">Check it out?</a>
+  Contact me at aldahir@protonmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio: It's made with Node.js and Express, which are technologies for backend development in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  these technologies by watching some of my tutorials <a href="https://youtube.com/@aldahirdev" target="_blank">here</a>.`,
+  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French, by the way).
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit AldahirDev's portfolio, just close the tab.`,
+};
