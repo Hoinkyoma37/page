1f48ad6d33b5b2d16cf360a93013aee86f146c0b
Aldahir <oyuri4675@gmail.com>
2025-01-12 10:44:23 -0500

    up

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,7 @@ export const dialogueData = {
    <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="" target="_blank">Check it out?</a>
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/Hoinkyoma37/page/blob/main/public/CV%20-%20Lic.%20Miguel%20Aldahir%20Ramos%20P%C3%A9rez.pdf" target="_blank">Check it out?</a>
   Contact me at lic.aldahir.ramos@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas."`,
