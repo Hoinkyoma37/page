dbcd97da079b4e829b0476a91e1e13bb6aa3aa2b
Aldahir <oyuri4675@gmail.com>
2025-01-12 10:29:14 -0500

    resume updated

--- src/constants.js
+++ src/constants.js
@@ -11,9 +11,9 @@ export const dialogueData = {
    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  Contact me at lic.aldahir.ramos@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas." target="_blank">here</a>.`,
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas."`,
   library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
   exit: `If you want to exit Aldahir's portfolio, just close the tab.`,
